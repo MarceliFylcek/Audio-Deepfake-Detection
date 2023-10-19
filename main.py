0512581fc3d35e4cc58b07e1f1db1482afa2f440
meh0w <michal.gruba00@gmail.com>
2023-10-20 01:54:13 +0200

    Added basic metric logging

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import os
 import wandb
 from models import CNNModel
 import torch.optim as optim
+from sklearn.metrics import classification_report
 
-# # start a new wandb run to track this script
+# # # start a new wandb run to track this script
 wandb.init(
     # set the wandb project where this run will be logged
     project="audio-deepfake-detection",
@@ -37,25 +38,26 @@ dataset = FakeAudioDataset(real_folder, fake_folder, 4_000, 16_000, 64)
 dataloader = DataLoader(dataset, batch_size=2,
                         shuffle=True, num_workers=0)
 
-for sample in dataloader:
-    raw_data, label = sample
-    print(raw_data.shape, label)
-    
-
-inputs = torch.unsqueeze(raw_data, 1)
-m = CNNModel(16, inputs.shape[2:])
-m.zero_grad()
-out = m(inputs)
-criterion = torch.nn.MSELoss()
-optimizer = optim.SGD(m.parameters(), lr=0.01)
+d_sample, _ =  next(iter(dataloader))
+m = CNNModel(16, torch.unsqueeze(d_sample, 1).shape[2:])
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(m.parameters(), lr=0.001)
 for epoch in range(10):
     optimizer.zero_grad()   # zero the gradient buffers
-    output = m(inputs)
-    target = torch.zeros(out.shape)
-    target[torch.arange(0,label.shape[0]), label] = 1
-    loss = criterion(output, target)
-    loss.backward()
-    optimizer.step()
-    wandb.log({"loss": loss})
+    for sample in dataloader:
+        raw_data, label = sample
+        inputs = torch.unsqueeze(raw_data, 1)
+        output = m(inputs)
+        target = torch.zeros(output.shape)
+        target[torch.arange(0,label.shape[0]), label] = 1
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
 
-print('a')
+
+        report = classification_report(torch.argmax(output, 1), torch.argmax(target, 1), output_dict=True)
+
+        for class_name in list(report.keys())[:-3]:
+            for metric in list(report[class_name].keys())[:-1]:
+                wandb.log({f'{class_name}_{metric}': report[class_name][metric]})
+                print(f'{class_name}_{metric}: {report[class_name][metric]}')
