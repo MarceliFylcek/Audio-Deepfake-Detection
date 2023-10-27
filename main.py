bda9c923b5686e4a593a7c2d6032c01222ac100c
CamaroTheBOSS <plessico@wp.pl>
2023-10-27 16:19:37 +0200

    Now loss is independent from batch_size, added parse option to turn off wandb, added GPU usage to train loop, added scikit-learn to requirements

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ Folder structure:
     /FAKE
 """
 
-
 if __name__ == "__main__":
     # Parse input arguments
     args = train_options.parse()
@@ -36,19 +35,26 @@ if __name__ == "__main__":
     checkpoint_freq = args.checkpoint_freq
     model_name = args.name
     no_valid = args.no_valid
+    wandb_disabled = args.disable_wandb
+
+    if not os.path.exists(MODELS_DIR):
+        os.makedirs(MODELS_DIR)
 
     # start a new wandb run to track this script
-    wandb.init(
-        # set the wandb project where this run will be logged
-        project="audio-deepfake-detection",
-        # track hyperparameters and run metadata
-        config={
-            "learning_rate": lr,
-            "architecture": "CNN",
-            "dataset": "11labs",
-            "epochs": n_epochs,
-        },
-    )
+    if not wandb_disabled:
+        wandb.init(
+            # set the wandb project where this run will be logged
+            project="audio-deepfake-detection",
+            # track hyperparameters and run metadata
+            config={
+                "learning_rate": lr,
+                "architecture": "CNN",
+                "dataset": "11labs",
+                "epochs": n_epochs,
+            },
+            name=args.name
+        )
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
     # Get training and validation dataloader
     train_dataloader = get_dataloader(TRAIN_DIR, batch_size)
@@ -60,10 +66,10 @@ if __name__ == "__main__":
     # Batch is passed to the model
 
     # Create the model
-    m = CNNModel(n_filters=25, input_shape=[batch.shape[2], batch.shape[3]])
+    m = CNNModel(n_filters=25, input_shape=[batch.shape[2], batch.shape[3]]).to(device)
 
     # Pretrained model loading
-    if pretrained_name != None:
+    if pretrained_name is not None:
         try:
             path = os.path.join(MODELS_DIR, pretrained_name) + ".pth"
             m.load_state_dict(torch.load(path))
@@ -100,6 +106,10 @@ if __name__ == "__main__":
             # zero the gradient buffers
             optimizer.zero_grad()
 
+            # Data to device
+            batch = batch.to(device)
+            labels = labels.to(device)
+
             # Get the output
             output = m(batch)
 
@@ -113,7 +123,7 @@ if __name__ == "__main__":
             optimizer.step()
 
             # Update running loss
-            loss_train += loss.item()
+            loss_train += loss.item() / batch_size
 
             # Update description of the sub-progress bar
             train_epoch_progress.set_postfix(
@@ -137,16 +147,18 @@ if __name__ == "__main__":
             all_labels = []
 
             valid_epoch_progress = tqdm(
-            valid_dataloader, f"Epoch {epoch} (Valid)", leave=False
+                valid_dataloader, f"Epoch {epoch} (Valid)", leave=False
             )
 
             # No gradient calculation
             with torch.no_grad():
                 # Evaluation loop
                 for batch_idx, (batch, labels) in enumerate(valid_epoch_progress):
+                    batch = batch.to(device)
+                    labels = labels.to(device)
                     output = m(batch)
                     loss = criterion(output, labels)
-                    loss_valid += loss.item()
+                    loss_valid += loss.item() / batch_size
                     predicted = torch.argmax(output, axis=1)
                     all_predicted.extend(predicted.cpu().numpy())
                     all_labels.extend(labels.cpu().numpy())
@@ -155,38 +167,40 @@ if __name__ == "__main__":
 
                     # Update description of the sub-progress bar
                     valid_epoch_progress.set_postfix(
-                    Loss=f"{loss_valid / (batch_idx + 1):.4f}"
+                        Loss=f"{loss_valid / (batch_idx + 1):.4f}"
                     )
 
             loss_valid /= len(valid_dataloader)
             accuracy = 100 * correct_prediction / total
+            print(f"Accuracy: {accuracy}%")
 
             # Zero division error
             report = classification_report(
                 all_predicted, all_labels, output_dict=True, zero_division=0
             )
 
-            for class_name in list(report.keys())[:-3]:
-                for metric in list(report[class_name].keys())[:-1]:
-                    wandb.log(
-                        {f"{class_name}_{metric}": report[class_name][metric]}
-                    )
-                    # print(f'{class_name}_{metric}: {report[class_name][metric]}')
+            if not wandb_disabled:
+                for class_name in list(report.keys())[:-3]:
+                    for metric in list(report[class_name].keys())[:-1]:
+                        wandb.log(
+                            {f"{class_name}_{metric}": report[class_name][metric]}
+                        )
+                        # print(f'{class_name}_{metric}: {report[class_name][metric]}')
 
-            wandb.log(
-                {
-                    "loss_train": loss_train,
-                    "loss_valid": loss_valid,
-                    "accuracy": accuracy,
-                }
-            )
+                wandb.log(
+                    {
+                        "loss_train": loss_train,
+                        "loss_valid": loss_valid,
+                        "accuracy": accuracy,
+                    }
+                )
 
         # Save the model
         if epoch % checkpoint_freq == 0:
             try:
                 path = (
-                    os.path.join(MODELS_DIR, model_name + "_e" + str(epoch))
-                    + ".pth"
+                        os.path.join(MODELS_DIR, model_name + "_e" + str(epoch))
+                        + ".pth"
                 )
                 torch.save(m.state_dict(), path)
             except Exception as e:
